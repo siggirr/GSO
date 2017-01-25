c6ac4a295d92135d884bbfdb8a14dcf7f90159e2
Sigurður Róbert Rúnarsson <sigurdurru219@nemi.tskoli.is>
2017-01-25 14:45:56 +0000

    3 auka línur og lokar aftur

--- Forrit.py
+++ Forrit.py
@@ -2,3 +2,7 @@
 txtfile = open("testfile.txt","w")
 txtfile.write ("Echo world")
 txtfile.close()
+txtfile = open("testfile.txt","a")
+txtfile.write("This is my second line")
+txtfile.write("This is THE 3rd line")
+txtfile.close()
